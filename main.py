511e6073c001acfd3bbecbe42164f76b3722392f
Andre Goranico Samosir <abrammarba@gmail.com>
2021-06-16 22:38:59 +0700

    Commit

--- main.py
+++ main.py
@@ -84,14 +84,18 @@ def scraper(s_url):
         except Exception:
             location = 'none'
 
+        try:
+            review_badge_title = s_soup.find_all('div', {'class': 'review-badge-title'})[1].text
+        except Exception:
+            review_badge_title = 'none'
+
         if scraper_testing is True:
             print('Today:', datetime.datetime.now().date())
-            print('\n')
-            print('Property Name:', property_name)
+            print('\nProperty Name:', property_name)
             print('Property Type:', property_type)
             print('Location:', location)
-            print('\n')
-            print('Sleep: 10')  # ==================================================================================
+            print('Review Badge Title:', review_badge_title)
+            print('\nSleep: 10')  # ==================================================================================
             time.sleep(10)  # ======================================================================================
 
         return [property_name, property_type, location]
